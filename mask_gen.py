83736700c917325ed419a10d8b6219ce475f4f75
lgm0905 <lgm0905@snu.ac.kr>
2021-06-11 00:09:04 +0900

    Add resources

--- mask_gen.py
+++ mask_gen.py
@@ -1,4 +1,16 @@
 from PIL import Image
 
-img = Image.new(mode='RGB', size=(1296, 1944), color=0xFFFFFF)
-img.save('./data/sample/shader/mask.png', format='PNG')
+# image resize
+def create_img(name, out_num) :
+    i = Image.open('./data/dpr/obama/{}.jpg'.format(name), mode='r')
+    j = i.resize((256,256), resample=0)
+    j.save('./data/dpr/obama/out{}.png'.format(out_num), format='PNG')
+
+# mask gen
+def create_mask() :
+    img = Image.new(mode='RGB', size=(256, 256), color=0xFFFFFF)
+    img.save('./data/dpr/obama/mask.png', format='PNG')
+
+for i in range(7) : 
+    create_img('obama_0{}'.format(str(i)), str(i))
+create_mask()
